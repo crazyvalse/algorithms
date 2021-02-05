c7c80e065392e3d4b863de9daf6368ec7d4b69d1
IsaacGu <crazyvalse@yeah.net>
2021-02-05 16:49:37 +0800

    快速排序

--- push/01-sort/2-kuais/old/2021-0205-2/01.js
+++ push/01-sort/2-kuais/old/2021-0205-2/01.js
@@ -3,30 +3,24 @@ function swap(array, a, b) {
 }
 
 /**
- * 1. 返回 pivot - 1
- * 2. 以 array[left] 为 初始 pivot，next = left + 1，
- * 3. 当 pivot > array[i] 的时候交换 swap(array, next, i)
- * 4. 最后 swap(array, left, pivot -1)
- *
- * pivot = array[left]
- * next = left + 1
- * for i = (left + 1),...,(right - 1);
- *  if pivot > array[i]
- *    swap(next, i)
- *
- * swap(left, pivot - 1)
- * return pivot - 1
+ * 1. 返回 Pivot 的索引
+ * 2. 以 array[left] 作为基准，if(array[i] < flag) array[point++] <> array[i] 交换位置
+ * 3. array[left] 与 array[point -1] 交换位置
  */
 const sortAndFindPivot = function (array, left, right) {
+  if (Array.isArray(array) && array.length < 2) {
+    return array
+  }
   const pivot = array[left]
   let next = left + 1
-  for (let i = left + 1; i < right; i++) {
-    if (pivot > array[i]) {
-      swap(array, next++, i)
+  for (let i = left + 1; i <= right; i++) {
+    if (array[i] < pivot) {
+      swap(array, next, i)
+      next++
     }
   }
-  swap(array, left, pivot - 1)
-  return pivot - 1
+  swap(array, left, next - 1)
+  return next - 1
 }
 
 /**
@@ -36,7 +30,7 @@ const sortAndFindPivot = function (array, left, right) {
  */
 function sort(array) {
   const walk = function (array, left, right) {
-    if (left <= right) {
+    if (left < right) {
       const pivot = sortAndFindPivot(array, left, right)
       walk(array, left, pivot)
       walk(array, pivot + 1, right)
