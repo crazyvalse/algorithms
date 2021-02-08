f4030d3f55811688f06e20470f64d6569707f961
IsaacGu <crazyvalse@yeah.net>
2021-02-08 17:42:04 +0800

    link 列表

---
+++ push/05-array/or/88.2.test.js
@@ -0,0 +1,9 @@
+const merge = require('./88.2.js')
+
+test('[1, 2, 2, 3, 5, 6]', () => {
+  expect(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)).toEqual([1, 2, 2, 3, 5, 6])
+})
+
+test('[1]', () => {
+  expect(merge([1], 1, [], 0)).toEqual([1])
+})
