02bff564405dcae214ada66d5007bb9b02515e51
IsaacGu <crazyvalse@yeah.net>
2021-02-18 17:13:32 +0800

    739

--- push/08-stack/739/or/02.js
+++ push/08-stack/739/or/02.js
@@ -1,14 +1,31 @@
+/**
+ * 用单调栈
+ * T:     [73, 74, 75, 71, 69, 72, 76, 73]
+ *
+ * stack: [75, 71, 69, 72]
+ *
+ * 1. 栈
+ *  - 当 array[i] <= stack[stack.length - 1] 说明不是，stack.push(i)
+ *  - 相反 新值大于栈中的，说明是
+ *    while(stack.length !== 0 && array[i] > stack[stack.length - 1]) {
+ *      result[index] = i - index
+ *      index = stack.pop()
+ *    }
+ * @param T
+ * @returns {any[]}
+ */
 const dailyTemperatures = (T) => {
-  const res = new Array(T.length).fill(0)
+  const result = new Array(T.length).fill(0)
   const stack = []
-  for (let i = T.length - 1; i >= 0; i--) {
-    while (stack.length && T[i] >= T[stack[stack.length - 1]]) {
-      stack.pop()
-    }
-    if (stack.length) {
-      res[i] = stack[stack.length - 1] - i
+  let index
+  for (let i = 0; i < T.length; i++) {
+    while (stack.length !== 0 && T[i] > T[stack[stack.length - 1]]) {
+      index = stack.pop()
+      result[index] = i - index
     }
     stack.push(i)
   }
-  return res
+  return result
 }
+
+module.exports = dailyTemperatures
