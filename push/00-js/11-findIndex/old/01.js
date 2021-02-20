0c550a18a1c5cf0d55dc2f617030857e6a101bd9
IsaacGu <crazyvalse@yeah.net>
2021-02-20 17:55:00 +0800

    findIndex

--- push/00-js/11-findIndex/old/01.js
+++ push/00-js/11-findIndex/old/01.js
@@ -1,5 +1,5 @@
 module.exports = function (array, fn) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0, length = array.length; i < length; i++) {
     if (fn.call(null, array[i], i, array)) {
       return i
     }
