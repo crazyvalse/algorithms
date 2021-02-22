9d1243adc46c8d4597209998aad72d6d438b5d73
IsaacGu <crazyvalse@yeah.net>
2021-02-22 13:35:28 +0800

    56

--- push/01-sort/56/check/01.js
+++ push/01-sort/56/check/01.js
@@ -1,5 +1,6 @@
 /**
- 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+ 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 
  示例 1：
  输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
@@ -13,32 +14,24 @@
 
  [[1, 9], [2, 5], [19, 20], [10, 11], [12, 20], [0, 3], [0, 1], [0, 2]]
  ------------------------------------------------------------------------------------------------------------------------
- 我们用数组 merged 存储最终的答案。
- 首先，我们将列表中的区间按照左端点升序排序。然后我们将第一个区间加入 merged 数组中，并按顺序依次考虑之后的每个区间：
- 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，我们可以直接将这个区间加入数组 merged 的末尾；
- 否则，它们重合，我们需要用当前区间的右端点更新数组 merged 中最后一个区间的右端点，将其置为二者的较大值。
-
- 主要的思想就是
-  - 如果 array[i][0] 相同，只需要找到 array[i][1] 最大的那个即可
-  - 如果不同，判断 merge[merge.length - 1][1] (右侧) < array[i][0] 新开一个，否则 判断 array[i][1] 与 merge[merge.length - 1][1] 最大的一个
-
- 1. 排序
- 2. 遍历排序后的数组
-  -
+ 1. 排序第一个元素 sort(array[0])
+ 2. 判断下一组第一个值比merged数组中的最后一个值的第二个值大的话 if(array[i][0] > merged[last][1])， merged.push()
+  - 否则 merged.push()
  * @param {number[][]} intervals
  * @return {number[][]}
  */
 const merge = function (intervals) {
+  // 排序
   intervals.sort((a, b) => a[0] - b[0])
-  const merged = []
-  for (let i = 0, length = intervals.length; i < length; i++) {
-    if (merged.length === 0 || merged[merged.length - 1][1] < intervals[i][0]) {
-      merged.push(intervals[i])
+  const result = []
+  for (let i = 0; i < intervals.length; i++) {
+    if (result.length === 0 || intervals[i][0] > result[result.length - 1][1]) {
+      result.push(intervals[i])
     } else {
-      merged[merged.length - 1][1] = Math.max(intervals[i][1], merged[merged.length - 1][1])
+      result[result.length - 1][1] = Math.max(result[result.length - 1][1], intervals[i][1])
     }
   }
-  return merged
+  return result
 }
 
 module.exports = merge
