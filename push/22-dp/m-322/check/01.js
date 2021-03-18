d77b0da2410881d5008c18fd99fd836c4cb34ecb
IsaacGu <crazyvalse@yeah.net>
2021-03-18 17:20:49 +0800

    dp

--- push/22-dp/m-322/check/01.js
+++ push/22-dp/m-322/check/01.js
@@ -38,17 +38,17 @@
  * @return {number}
  */
 var coinChange = function (coins, amount) {
-  let dp = new Array(amount).fill(amount + 1)
+  const dp = new Array(amount + 1).fill(amount + 1)
   dp[0] = 0
-  for (let i = 1; i <= amount; i++) {
-    for (let j = 0; j < coins.length; j++) {
-      if (coins[j] <= i) {
-        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1)
+  // 金额的数量
+  for (let i = 0; i <= amount; i++) {
+    for (let coin of coins) {
+      if (coin <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1)
       }
     }
   }
-
-  return dp[amount] > amount ? -1 : dp[amount]
+  return dp[amount] === amount + 1 ? -1 : dp[amount]
 }
 
 module.exports = coinChange
--- push/22-dp/m-322/or/01.js
+++ push/22-dp/m-322/or/01.js
@@ -38,7 +38,7 @@
  * @return {number}
  */
 var coinChange = function (coins, amount) {
-  let dp = new Array(amount).fill(amount + 1)
+  let dp = new Array(amount + 1).fill(amount + 1)
   dp[0] = 0
   for (let i = 1; i <= amount; i++) {
     for (let j = 0; j < coins.length; j++) {
